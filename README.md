398d6710df83e0bb3ca180865792939c70ea0030
Rubén Cortés Muñoz <DAM220@iesmhp.local>
2024-02-26 18:04:13 +0100

    README añadido, aplicacion de python terminada

---
+++ README.md
@@ -0,0 +1,7 @@
+Aplicacion Escritorio:https://github.com/Kangelx/AplicacionEscritorio-Proyecto-2024-2025/blob/main/README.md
+
+Aplicación dis positivo Móvil: https://github.com/Kangelx/AplicacionMovil-Proyecto-2024-2025.git
+
+Base de datos: https://github.com/Kangelx/Base-de-datos-Proyecto-2024-2025/blob/main/README.md
+
+Documentacion: https://github.com/Kangelx/Documentacion-Proyecto-2024-2025
--- parteSGE/Main.py
+++ parteSGE/Main.py
@@ -2,50 +2,62 @@ import os
 import pickle
 import time
 import requests
-import json
-
-# def hacer_post(url, datos):
-#     try:
-#
-#         respuesta = requests.post(url, json=datos)
-#         respuesta.raise_for_status()  # Lanza una excepción si la solicitud no es exitosa (código de estado distinto de 2xx)
-#         return respuesta.json()  # Devuelve la respuesta en formato JSON si la solicitud es exitosa
-#     except requests.exceptions.RequestException as e:
-#         print("Error al hacer la solicitud:", e)
-#         return None
-#
-# url = "http://localhost:4001/login"
-# datos = {"username": "m","password": "m"}  # Aquí deberías proporcionar los datos que deseas enviar en el POST
-# respuesta = hacer_post(url, datos)
-# if respuesta:
-#     print("Respuesta recibida:", respuesta)
-# else:
-#     print("No se pudo obtener respuesta.")
-
-# i=1
-# list_names=list()
-# try:
-#     while True:
-#         url='http://localhost:4001/incidencia/{}'.format(i)
-#         r=requests.get(url)
-#         # j=r.json()
-#         # name=j['descripcion']
-#         print(r)
-#         i+=1
-# except KeyError:
-#     print()
-
 from Incidencia import Incidencia
+
 horas=0
 minutos=0
 segundos=0
 booleano=False
+# este metodo verifica que se hayan introducido las credenciales correctamente y devuelve
+# en formato json toda la informacion de la incidencia con ID pasado por parameto.
+def hacer_post(url, datos, token,i):
+    headers = {"Authorization": "Bearer " + token}
+    try:
+        respuesta= requests.get("http://localhost:4001/incidencia/{}".format(i),json=datos,headers=headers)
+        respuesta.raise_for_status()
+        return respuesta.json()
+    except requests.exceptions.RequestException as e:
+        print("Error al hacer la solicitud:", e)
+        return None
 
+# metodo que verifica que se hayan introducido las credenciales correctamente
+# y devuelve en formato json toda la informacion de todas las incidencias.
+def devolverTotal(url, datos, token):
+    headers = {"Authorization": "Bearer " + token}
+    try:
+        respuesta= requests.get("http://localhost:4001/incidencia",json=datos,headers=headers)
+        respuesta.raise_for_status()
+        return respuesta.json()
+    except requests.exceptions.RequestException as e:
+        print("Error al hacer la solicitud:", e)
+        return None
+
+# contiene el enlace de la pagina de login de la API.
+url = "http://localhost:4001/login"
+# contiene el nombre de usuario y la contraseña para iniciar sesion
+datos = {"username": "m","password": "m"}  # Credenciales del encargado
+# contiene el token necesario para acceder a la API.
+token = "eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6WyJhZG1pbmlzdHJhZG9yIl0sInN1YiI6Im0iLCJpYXQiOjE3MDg3MTQwODAsImV4cCI6MTcwOTcxNDA4MH0.3nIZEm-VzzfO2kZo05v5LyGB5jYqO2W5HQPVlp3F0FIdRvc29c5dZX3p1_4dEh9rb4EwVkyPWcd1TytRBNiS0g"
+
+# muestra todas las incidencias
+i=1
+Total=len(devolverTotal(url,datos,token))
+while i!=Total:
+    respuesta = hacer_post(url, datos,token,i)
+    i+=1
+    if respuesta:
+        print("Respuesta recibida:", respuesta)
+    else:
+        Total+=1
+
+# abre el fichero, si no existe lo crea en el directorio de trabajo.
 absFilePath = os.path.abspath(__file__)
 Path , filename =os.path.split(absFilePath)
-# crea el fichero si no existe
 with open("Incidencias.pickle", "ab+") as f:
      pass
+
+# pide el ID de una incidencia, si la incidencia no existe escribe un mensaje y termina el programa
+# controla que solo puedan introducirse numeros.
 while True:
     try:
         print("Id de incidencia")
@@ -54,56 +66,74 @@ while True:
     except ValueError:
         print("El ID debe ser numerico")
 
-print(f"Reparando incidencia {id}")
-inicio = time.time()
-print("Cerrar incidencia (terminado? si/no)")
-terminado=input()
-segundos=time.time()-inicio;
+respuesta = hacer_post(url, datos,token,id)
+if respuesta:
+    # empieza a cronometrar el tiempo en la incidencia.
+    print(f"Reparando incidencia {id}")
+    inicio = time.time()
 
-if terminado.upper()=="si".upper():
-    booleano=True
+    # Termina de cronometrar el tiempo en la incidencia
+    # controla que solo se pueda escribir "si" o "no".
+    while True:
+        try:
+            print("Cerrar incidencia (terminado? si/no)")
 
-listaIncidencias=[]
-with open("Incidencias.pickle", "rb") as f:
-    try:
-        while True:
-            listaIncidencias.append(pickle.load(f))
-    except EOFError:
-        pass
+            terminado=input()
+            if terminado.upper()=="si".upper() or terminado.upper()=="no".upper():
+                break
+        except ValueError:
+            pass
 
-Encontrado=False
-incidenciaAux=Incidencia()
-for v in listaIncidencias:
-    if v.getIdIncidencia() == id:
-        Encontrado=True
-        incidenciaAux=v
+    segundos=time.time()-inicio;
+    if terminado.upper()=="si".upper():
+            booleano=True
 
-if Encontrado==True:
-    segundos=segundos+(int(incidenciaAux.getHoras())*3600)+(int(incidenciaAux.getMinutos())*60)+int(incidenciaAux.getSegundos())
-    listaIncidencias.remove(incidenciaAux)
-    if segundos>=60:
-        minutos=segundos/60
-        segundos=segundos%60
-        if minutos>=60:
-            horas=minutos/60
-            minutos=minutos%60
-    incidencia=Incidencia(id,booleano,int("{:.0f}".format(horas)),int("{:.0f}".format(minutos)),int("{:.0f}".format(segundos)))
-    listaIncidencias.append(incidencia)
-    print(f"Tiempo total en esta incidencia: {horas:.0f}:{minutos:.0f}:{segundos:.0f}")
+    # abre el fichero y carga todas las incidencias en una lista.
+    listaIncidencias=[]
+    with open("Incidencias.pickle", "rb") as f:
+        try:
+            while True:
+                listaIncidencias.append(pickle.load(f))
+        except EOFError:
+            pass
+
+    Encontrado=False
+    incidenciaAux=Incidencia()
+    for v in listaIncidencias:
+        if v.getIdIncidencia() == id:
+            Encontrado=True
+            incidenciaAux=v
+
+    # si la incidencia existe suma el tiempo de esta sesion al escrito en el fichero y lo actualiza
+    # si la incidencia no existe se registra una incidencia nueva en el fichero.
+    if Encontrado==True:
+        segundos=segundos+(int(incidenciaAux.getHoras())*3600)+(int(incidenciaAux.getMinutos())*60)+int(incidenciaAux.getSegundos())
+        listaIncidencias.remove(incidenciaAux)
+        if segundos>=60:
+            minutos=segundos/60
+            segundos=segundos%60
+            if minutos>=60:
+                horas=minutos/60
+                minutos=minutos%60
+        incidencia=Incidencia(id,booleano,int("{:.0f}".format(horas)),int("{:.0f}".format(minutos)),int("{:.0f}".format(segundos)))
+        listaIncidencias.append(incidencia)
+        print(f"Tiempo total en esta incidencia: {horas:.0f}:{minutos:.0f}:{segundos:.0f}")
+    else:
+        if segundos>=60:
+            minutos=segundos/60
+            segundos=segundos%60
+            if minutos>=60:
+                horas=minutos/60
+                minutos=minutos%60
+        incidencia=Incidencia(id,booleano,int("{:.0f}".format(horas)),int("{:.0f}".format(minutos)),int("{:.0f}".format(segundos)))
+        listaIncidencias.append(incidencia)
+        print(f"Tiempo total en esta incidencia: {horas:.0f}:{minutos:.0f}:{segundos:.0f}")
+
+    # guarda los objetos en un archivo
+    with open("Incidencias.pickle", "wb") as f:
+        for aux in listaIncidencias:
+            pickle.dump(aux,f)
+
+    f.close()
 else:
-    if segundos>=60:
-        minutos=segundos/60
-        segundos=segundos%60
-        if minutos>=60:
-            horas=minutos/60
-            minutos=minutos%60
-    incidencia=Incidencia(id,booleano,int("{:.0f}".format(horas)),int("{:.0f}".format(minutos)),int("{:.0f}".format(segundos)))
-    listaIncidencias.append(incidencia)
-    print(f"Tiempo total en esta incidencia: {horas:.0f}:{minutos:.0f}:{segundos:.0f}")
-
-# guarda los objetos en un archivo
-with open("Incidencias.pickle", "wb") as f:
-    for aux in listaIncidencias:
-        pickle.dump(aux,f)
-
-f.close()
+    print("la incidencia no existe")
